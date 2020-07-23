a9718dc9a0c1af1f046abc726de54718ec1b8a9a
Pedestrian1671022 <1172012891@qq.com>
2020-07-23 13:05:37 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,310 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+
+from timeit import time
+import warnings
+import cv2
+import random
+import numpy as np
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+import imutils.video
+from deep_sort.videocaptureasync import VideoCaptureAsync
+
+from ctypes import *
+
+def sample(probs):
+    s = sum(probs)
+    probs = [a / s for a in probs]
+    r = random.uniform(0, 1)
+    for i in range(len(probs)):
+        r = r - probs[i]
+        if r <= 0:
+            return i
+    return len(probs) - 1
+
+
+def c_array(ctype, values):
+    arr = (ctype * len(values))()
+    arr[:] = values
+    return arr
+
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int)]
+
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+
+lib = CDLL("model_data/libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+predict = lib.network_predict
+predict.argtypes = [c_void_p, POINTER(c_float)]
+predict.restype = POINTER(c_float)
+
+set_gpu = lib.cuda_set_device
+set_gpu.argtypes = [c_int]
+
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+get_network_boxes = lib.get_network_boxes
+get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
+get_network_boxes.restype = POINTER(DETECTION)
+
+make_network_boxes = lib.make_network_boxes
+make_network_boxes.argtypes = [c_void_p]
+make_network_boxes.restype = POINTER(DETECTION)
+
+free_detections = lib.free_detections
+free_detections.argtypes = [POINTER(DETECTION), c_int]
+
+free_ptrs = lib.free_ptrs
+free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+network_predict = lib.network_predict
+network_predict.argtypes = [c_void_p, POINTER(c_float)]
+
+reset_rnn = lib.reset_rnn
+reset_rnn.argtypes = [c_void_p]
+
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+do_nms_obj = lib.do_nms_obj
+do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+do_nms_sort = lib.do_nms_sort
+do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+free_image = lib.free_image
+free_image.argtypes = [IMAGE]
+
+letterbox_image = lib.letterbox_image
+letterbox_image.argtypes = [IMAGE, c_int, c_int]
+letterbox_image.restype = IMAGE
+
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+load_image = lib.load_image_color
+load_image.argtypes = [c_char_p, c_int, c_int]
+load_image.restype = IMAGE
+
+rgbgr_image = lib.rgbgr_image
+rgbgr_image.argtypes = [IMAGE]
+
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    w = int(round(w))
+    ymin = int(round(y - (h / 2)))
+    h = int(round(h))
+    return [xmin, ymin, w, h]
+
+
+def array_to_image(arr):
+    # need to return old values to avoid python freeing memory
+    arr = arr.transpose(2, 0, 1)
+    c, h, w = arr.shape[0:3]
+    arr = np.ascontiguousarray(arr.flat, dtype=np.float32) / 255.0
+    data = arr.ctypes.data_as(POINTER(c_float))
+    im = IMAGE(w, h, c, data)
+    return im, arr
+
+
+def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    im, image = array_to_image(image)
+    rgbgr_image(im)
+    num = c_int(0)
+    pnum = pointer(num)
+    predict_image(net, im)
+    dets = get_network_boxes(net, im.w, im.h, thresh,
+                             hier_thresh, None, 0, pnum)
+    num = pnum[0]
+    if nms: do_nms_obj(dets, num, meta.classes, nms)
+
+    confidences = []
+    boxes = []
+    for j in range(num):
+        a = dets[j].prob[0:meta.classes]
+        if any(a):
+            ai = np.array(a).nonzero()[0]
+            for i in ai:
+                b = dets[j].bbox
+                confidences.append(dets[j].prob[i])
+                boxes.append([b.x, b.y, b.w, b.h])
+    return boxes, confidences
+
+warnings.filterwarnings('ignore')
+
+def main():
+
+    # Definition of the parameters
+    max_cosine_distance = 0.3
+    nn_budget = None
+    nms_max_overlap = 1.0
+    
+    # Deep SORT
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename,batch_size=1)
+    
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    writeVideo_flag = True
+    asyncVideo_flag = False
+
+    file_path = 'model_data/input_video.avi'
+    if asyncVideo_flag :
+        video_capture = VideoCaptureAsync(file_path)
+    else:
+        video_capture = cv2.VideoCapture(file_path)
+
+    if asyncVideo_flag:
+        video_capture.start()
+
+    if writeVideo_flag:
+        if asyncVideo_flag:
+            w = int(video_capture.cap.get(3))
+            h = int(video_capture.cap.get(4))
+        else:
+            w = int(video_capture.get(3))
+            h = int(video_capture.get(4))
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('model_data/output_yolov3.mp4', fourcc, 25, (w, h))
+        frame_index = -1
+
+    fps = 0.0
+    fps_imutils = imutils.video.FPS().start()
+
+    net = load_net(b"model_data/yolov3-tiny.cfg",
+                   b"model_data/yolov3-tiny.weights",
+                   0)
+    meta = load_meta(b"model_data/coco.data")
+
+    while True:
+        ret, frame = video_capture.read()  # frame shape 640*480*3
+        if ret != True:
+             break
+
+        t1 = time.time()
+
+        # frame = cv2.resize(frame, (416, 416))
+        boxes, confidences = detect(net, meta, frame)
+        print(boxes)
+        # print(boxes, confidences)
+        boxes = [convertBack(float(box[0]), float(box[1]), float(box[2]), float(box[3])) for box in boxes]
+        # boxes, confidences = yolo.detect_image(image)
+        # print(boxes)
+        # print(confidences)
+
+        # boxes = np.array(boxes)
+        # confidences = np.array(confidences)
+        #
+        # # Run non-maxima suppression.
+        # indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, confidences)
+        #
+        # boxes = [boxes[i] for i in indices]
+        # confidences = [confidences[i] for i in indices]
+
+        # time1 = time.time()
+
+        features = encoder(frame, boxes)
+
+        # time2 = time.time()
+
+        detections = [Detection(bbox, confidence, feature) for bbox, confidence, feature in zip(boxes, confidences, features)]
+
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+
+        # time3 = time.time()
+        
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue 
+            bbox = track.to_tlbr()
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+
+        for det in detections:
+            bbox = det.to_tlbr()
+            score = "%.2f" % round(det.confidence * 100, 2)
+            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+            cv2.putText(frame, score + '%', (int(bbox[0]), int(bbox[3])), 0, 5e-3 * 130, (0,255,0),2)
+            
+        cv2.imshow('', frame)
+
+        if writeVideo_flag: # and not asyncVideo_flag:
+            # save a frame
+            out.write(frame)
+            frame_index = frame_index + 1
+
+        fps_imutils.update()
+
+        fps = (fps + (1./(time.time()-t1))) / 2
+        print("FPS = %f"%(fps))
+
+        # time4 = time.time()
+        #
+        # time_sum = time4 - t1
+        # print("time:", (time1-t1)/time_sum, (time2-time1)/time_sum, (time3-time2)/time_sum, (time4-time3)/time_sum)
+        
+        # Press Q to stop!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    fps_imutils.stop()
+    print('imutils FPS: {}'.format(fps_imutils.fps()))
+
+    if asyncVideo_flag:
+        video_capture.stop()
+    else:
+        video_capture.release()
+
+    if writeVideo_flag:
+        out.release()
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
